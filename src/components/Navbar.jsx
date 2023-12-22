0651efd990c7418bd39728707246d6a980bcb2b3
Miran <mirankavinda12@gmail.com>
2023-12-22 17:00:30 +0530

    Add NewsBoard component and update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">News Magazine</a>
+    <a className="navbar-brand" href="#"><span className="badge bg-light text-dark fs-5">News Magazine</span></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
